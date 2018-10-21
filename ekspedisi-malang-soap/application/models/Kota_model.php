e0663453c639cc6be268e6a2724f7975365db525
jelitasm <jelitasekarmelati@gmail.com>
2018-10-21 19:44:31 +0700

    soap server + client malang crud Kota

---
+++ ekspedisi-malang-soap/application/models/Kota_model.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kota_model extends CI_Model {
+
+	var $soap_client;
+	public function __construct()
+	{
+		$this->soap_client = new SoapClient($this->apidata->get_api_malang()."/MySoapServer?wsdl");
+		parent::__construct();
+	}
+	public function get()
+	{
+		return  json_decode($this->soap_client->getData(json_encode(array("table"=>"kota"))));
+	}
+	public function get_id($id)
+	{
+		return json_decode($this->soap_client->getDataId(json_encode(array("table"=>"kota",'id'=>$id))));
+	}
+	public function insert()
+	{
+		$set = array(
+			'kota' => $this->input->post('kota'),
+			'harga' => $this->input->post('harga'),
+		);
+		$data = $this->soap_client->insertData(json_encode(array("table"=>"kota","data"=>$set)));
+	}
+	public function update($id)
+	{
+		$set = array(
+			'kota' => $this->input->post('kota'),
+			'harga' => $this->input->post('harga'),
+		);
+		$this->soap_client->updateData(json_encode(array("table"=>"kota","primary_key"=>"id","id"=>$id,"data"=>$set)));
+	}
+	public function delete($id)
+	{
+		$this->soap_client->deleteData(json_encode(array("table"=>"kota","primary_key"=>"id","id"=>$id)));
+	}
+}
